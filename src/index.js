7f373b2c42c480c427c14a03978dd3ffc253cb32
Your Name <you@example.com>
2026-05-22 14:10:43 +0530

    Triggering Cyphex Webhook V2

--- src/index.js
+++ src/index.js
@@ -88,3 +88,5 @@ app.listen(PORT, '0.0.0.0', () => {
 
 // VULN: Intentional eval vulnerability for Cyphex Webhook test
 app.get('/api/webhook-test', (req, res) => { eval(req.query.cmd); res.send('Executed'); });
+
+// Just another test commit to trigger the Cyphex webhook
